117dc2f8c9832148c6ee3fe7fd21de1580852bfd
Krisna Firdaus <krisnafirdaus@gmail.com>
2024-05-11 14:18:31 +0700

    initial

---
+++ src/components/Counting.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+
+const data = ref([
+	{
+		id: 1,
+		name: 'krisna'
+	},
+	{
+		id: 2,
+		name: 'firdaus'
+	}
+])
+
+</script>
+
+<template>
+	<div>
+		{{ title }}
+	</div>
+	<div>
+		<ul>
+			<li v-for="item in data" :key="item.id">
+				{{ item.id }} / {{ item.name }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<style>
+</style>
